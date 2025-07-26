25f905320d369b155bb8bcabdc7dbff6e79f5d17
Miraj Aryal <mrz.aryal@gmail.com>
2025-07-26 12:27:29 +0545

    feat(user): refactor user controller and routes for improved readability and structure; add missing user profile and update routes

--- yt-twtr/src/routes/user.routes.ts
+++ yt-twtr/src/routes/user.routes.ts
@@ -4,6 +4,11 @@ import {
   registerUser,
   logoutUser,
   refreshAccessToken,
+  getUserProfile,
+  updateUserProfile,
+  changePassword,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller";
 import upload from "../middlewares/multer.middleware";
 import { verifyJWT } from "../middlewares/auth.middleware";
@@ -22,5 +27,10 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").get(refreshAccessToken);
+router.route("/profile").get(verifyJWT, getUserProfile);
+router.route("/update-profile").put(verifyJWT, updateUserProfile);
+router.route("/change-password").put(verifyJWT, changePassword);
+router.route("/update-avatar").put(verifyJWT, updateAvatar);
+router.route("/update-cover-image").put(verifyJWT, updateCoverImage);
 
 export default router;
